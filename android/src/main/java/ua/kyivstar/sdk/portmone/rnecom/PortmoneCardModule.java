a2870c0954d7bbbcae305b7019cf02c5830e8685
Yevhenii Onipko <y.onipko@kyivstar.net>
2020-04-16 16:03:56 +0300

    added saving card with portmone sdk

--- android/src/main/java/ua/kyivstar/sdk/portmone/rnecom/PortmoneCardModule.java
+++ android/src/main/java/ua/kyivstar/sdk/portmone/rnecom/PortmoneCardModule.java
@@ -1,23 +1,36 @@
 package ua.kyivstar.sdk.portmone.rnecom;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
 
+import com.facebook.react.bridge.ActivityEventListener;
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.BaseActivityEventListener;
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+
+import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.uimanager.IllegalViewOperationException;
 import com.portmone.ecomsdk.PortmoneSDK;
+import com.portmone.ecomsdk.data.Bill;
 import com.portmone.ecomsdk.data.CardPaymentParams;
+import com.portmone.ecomsdk.data.SaveCardParams;
 import com.portmone.ecomsdk.data.style.AppStyle;
 import com.portmone.ecomsdk.ui.card.CardPaymentActivity;
+import com.portmone.ecomsdk.ui.savecard.PreauthCardActivity;
 import com.portmone.ecomsdk.util.Constant$BillCurrency;
 
 import com.portmone.ecomsdk.util.Constant$Language;
 
 import java.util.Arrays;
 import java.util.List;
+
+import static android.app.Activity.RESULT_OK;
 
 public class PortmoneCardModule extends ReactContextBaseJavaModule {
 
@@ -27,10 +40,12 @@ public class PortmoneCardModule extends ReactContextBaseJavaModule {
     );
 
     private ReactApplicationContext reactContext;
+    private Promise promise;
 
     public PortmoneCardModule(@NonNull ReactApplicationContext reactContext) {
         super(reactContext);
         this.reactContext = reactContext;
+        reactContext.addActivityEventListener(mActivityEventListener);
     }
 
     @Override
@@ -76,6 +91,44 @@ public class PortmoneCardModule extends ReactContextBaseJavaModule {
             );
         } catch (IllegalViewOperationException e) {
             Log.d("PORTMONE", "initCardPayment: ERROR", e);
+        }
+    }
+
+    @ReactMethod
+    public void initCardSaving(String payeeId) {
+        try {
+            final SaveCardParams saveCardParams = new SaveCardParams(
+                payeeId,
+                Constants.BILL_NUMBER,
+                ""
+            );
+            PreauthCardActivity.performTransaction(
+                getCurrentActivity(),
+                Constants.REQUEST_CODE,
+                saveCardParams
+            );
+        } catch (IllegalViewOperationException e) {
+            Log.d("PORTMONE", "initCardSaving: ERROR", e);
+        }
+    }
+
+    private final ActivityEventListener mActivityEventListener = new BaseActivityEventListener() {
+        @Override
+        public void onActivityResult(Activity activity, int requestCode, int resultCode, Intent intent) {
+            Log.d("PORTMONE", intent.toString());
+            switch (requestCode) {
+                case Constants.REQUEST_CODE:
+                    if (resultCode == RESULT_OK) {
+                        String message = intent.getStringExtra("");
+                        WritableMap map = Arguments.createMap();
+                        map.putString("token", message);
+                        promise.resolve(map);
+                    } else {
+                        promise.reject("PORTMONE", new Error("Result code: " + resultCode));
+                    }
+                    promise = null;
+                    break;
+            }
         }
     }
 }
