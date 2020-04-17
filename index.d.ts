516ab72f1db800ca6a1cd20bdc4fdca8e5394699
Yevhenii Onipko <y.onipko@kyivstar.net>
2020-04-17 12:47:09 +0300

    added types for index.js

---
+++ index.d.ts
@@ -0,0 +1,10 @@
+declare module 'react-native-ecom-portmone' {
+    class PortmoneCardModule {
+        static invokePortmoneSdk(): void;
+
+        static initCardPayment(): void;
+
+        static initCardSaving(): void;
+    }
+    export default PortmoneCardModule;
+}
--- index.js
+++ index.js
@@ -1,4 +1,35 @@
 import { NativeModules } from "react-native";
-const { PortmoneCardModule } = NativeModules;
+const { PortmoneCardModule: RNModule } = NativeModules;
 
-export default PortmoneCardModule;
+export default class PortmoneCardModule {
+  static invokePortmoneSdk(lang) {
+    try {
+      if (typeof lang !== 'string') {
+        throw new Error('lang must be string');
+      }
+      return RNModule.invokePortmoneSdk(lang);
+    } catch (e) {
+      console.info(`PortmoneCardModule => invokePortmoneSdk => ${e}`)
+    }
+  }
+  static initCardPayment(payeeId, phoneNumber, billAmount) {
+    try {
+      if (typeof payeeId !== 'string') {
+        throw new Error('payeeId must be string');
+      }
+      return RNModule.initCardPayment(payeeId, phoneNumber, billAmount);
+    } catch (e) {
+      console.info(`PortmoneCardModule => initCardPayment => ${e}`)
+    }
+  }
+  static initCardSaving(payeeId) {
+    try {
+      if (typeof payeeId !== 'string') {
+        throw new Error('payeeId must be string');
+      }
+      return RNModule.initCardSaving(payeeId);
+    } catch (e) {
+      console.info(`PortmoneCardModule => initCardSaving => ${e}`)
+    }
+  }
+};
